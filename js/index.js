c5f6980cc002059920c3246f815db8bc35e20604
Devin1996 <devinchandula@gmail.com>
2020-03-13 14:35:25 +0530

    Admin LogIn/Register and Password reset Completed

--- js/index.js
+++ js/index.js
@@ -19,14 +19,14 @@ $("#btn-login").click(function () {
 
     if (email != "" && password != "") {
         var result = firebase.auth().signInWithEmailAndPassword(email, password);
-        result.catch(function(error){
-            var errorCode =error.code;
+        result.catch(function (error) {
+            var errorCode = error.code;
             var errorMessage = error.message;
-            
+
             console.log(errorCode);
             console.log(errorMessage);
 
-            window.alert("Message: "+errorMessage);
+            window.alert("Message: " + errorMessage);
         });
     }
     else {
@@ -34,6 +34,63 @@ $("#btn-login").click(function () {
     }
 });
 
+$("#btn-signup").click(function () {
+    var email = $("#email").val();
+    var password = $("#password").val();
+    var confirmPassword = $("#confirmPassword").val();
+
+    if (email != "" && password != "" && confirmPassword != "") {
+        if (password == confirmPassword) {
+            var result = firebase.auth().createUserWithEmailAndPassword(email, password);
+            result.catch(function (error) {
+                var errorCode = error.code;
+                var errorMessage = error.message;
+
+                console.log(errorCode);
+                console.log(errorMessage);
+
+                window.alert("Message: " + errorMessage);
+            });
+        }
+        else {
+            window.alert("Password do not match !");
+        }
+
+    }
+    else {
+        window.alert("Form is Incomplete. Please fill out all fields.");
+    }
+});
+
+
 $("#btn-logout").click(function () {
     firebase.auth().signOut();
+});
+
+$("#btn-reset-pw").click(function () {
+    
+    var auth = firebase.auth();
+    var email = $("#email").val();
+
+    if(email != "")
+    {
+        auth.sendPasswordResetEmail(email).then(function()
+        {
+            window.alert("Email has been sent to you, Please check and verify.");
+        })
+        .catch(function(error)
+        {
+            var errorCode = error.code;
+            var errorMessage = error.message;
+
+            console.log(errorCode);
+            console.log(errorMessage);
+
+            window.alert("Message: " + errorMessage);
+        });
+
+    }
+    else{
+        window.alert("Please enter your registered email for account");
+    }
 });
