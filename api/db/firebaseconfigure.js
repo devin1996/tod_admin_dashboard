eacc457f6bc6573a9f2cb8965850af60423c327b
Devin1996 <devinchandula@gmail.com>
2020-03-15 22:28:06 +0530

    Retrieve Bus Route Data from the DB

--- api/db/firebaseconfigure.js
+++ api/db/firebaseconfigure.js
@@ -1,5 +1,4 @@
-var firebaseConfig =
-{
+var firebaseConfig = {
     apiKey: "AIzaSyDVSNwiJxw-godBSv-gzFzYgNUXZU81ksY",
     authDomain: "ptms-4b687.firebaseapp.com",
     databaseURL: "https://ptms-4b687.firebaseio.com",
@@ -7,8 +6,8 @@ var firebaseConfig =
     storageBucket: "ptms-4b687.appspot.com",
     messagingSenderId: "1028667957586",
     appId: "1:1028667957586:web:698a3ac97b71487e131a8b"
-};
-// Initialize Firebase
-var db = firebase.initializeApp(firebaseConfig);
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  firebase.analytics();
 
-module.exports = db;
--- api/routes/busRoutes.js
+++ api/routes/busRoutes.js
@@ -34,3 +34,34 @@ function insertRoute() {
   });
 }
 
+var database = firebase.database();
+          
+database.ref("routes").child("busRoutes").once("value", function(snapshot){
+  if (snapshot.exists()) {
+    var content = '';
+
+  snapshot.forEach(function (childSnapshot){
+
+    
+    var val  = childSnapshot.val();
+    
+    content += '<tr>';
+    content += '<td>'+val.routeRegNo+'</td>';
+    content += '<td>'+val.routeNo+'</td>';
+    content += '<td>'+val.routeName+'</td>';
+    content += '<td>'+val.totalDistance+'</td>';
+    content += '<td>'+val.busType+'</td>';
+    content += '<td>'+val.avgSpeed+'</td>';
+    content += '<td>'+val.avgTime+'</td>';
+    content += '<td>'+val.efectiveDate+'</td>';
+    content += '</tr>';
+
+  });
+
+
+   $('#table1').append(content);
+
+
+}
+
+});
